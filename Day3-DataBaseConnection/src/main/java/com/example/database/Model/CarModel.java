505288beb4bc0eda95127f8481cb298e1b398d6e
Hariharan M <hariabi6135@gmail.com>
2023-04-21 12:47:37 +0530

    imit

---
+++ Day3-DataBaseConnection/src/main/java/com/example/database/Model/CarModel.java
@@ -0,0 +1,35 @@
+package com.example.database.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Car")
+public class CarModel 
+{
+	@Id
+	@Column(name="cid")
+	private int cid;
+	private String cname;
+	private String cmodel;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public String getCmodel() {
+		return cmodel;
+	}
+	public void setCmodel(String cmodel) {
+		this.cmodel = cmodel;
+	}
+}
